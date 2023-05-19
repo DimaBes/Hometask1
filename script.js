08a4d17af0ff6bd885ae080e746921bd0a875a96
DimaBes <124096117+DimaBes@users.noreply.github.com>
2023-05-19 21:14:52 +0300

    HT

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-// Функция для определения количества дней в месяце
 function getDaysInMonth(year, month) {
   return new Date(year, month + 1, 0).getDate();
 }
-
-// Функция для генерации календаря
 function generateCalendar(year, month) {
   const calendar = document.querySelector('.calendar');
 
-  // Очищаем содержимое календаря
+function updateMonthText(year, month) {
+  const monthText = document.querySelector('.month');
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  monthText.textContent = `${monthNames[month]} ${year}`;
+}
+updateMonthText(currentYear, currentMonth);
+
+const prevMonthBtn = document.querySelector('.strelaLeft');
+const nextMonthBtn = document.querySelector('.strelaRight');
+
+prevMonthBtn.addEventListener('click', () => {
+  currentMonth--;
+  if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear--;
+  }
+  generateCalendar(currentYear, currentMonth);
+  updateMonthText(currentYear, currentMonth);
+});
+
+nextMonthBtn.addEventListener('click', () => {
+  currentMonth++;
+  if (currentMonth > 11) {
+    currentMonth = 0;
+    currentYear++;
+  }
+  generateCalendar(currentYear, currentMonth);
+  updateMonthText(currentYear, currentMonth);
+});
+
+
   calendar.innerHTML = '';
 
   const daysInMonth = getDaysInMonth(year, month);
   const firstDayOfWeek = new Date(year, month, 1).getDay();
   const prevMonth = month - 1 < 0 ? 11 : month - 1;
 
-  // Создаем пустые ячейки для дней предыдущего месяца
   for (let i = 0; i < firstDayOfWeek; i++) {
     const li = document.createElement('li');
     li.classList.add('empty');
@@ -23,15 +52,13 @@ function generateCalendar(year, month) {
     calendar.appendChild(li);
   }
 
-  // Создаем ячейки для дней текущего месяца
   for (let day = 1; day <= daysInMonth; day++) {
     const li = document.createElement('li');
     li.textContent = day;
     calendar.appendChild(li);
   }
 
-  // Создаем пустые ячейки для дней следующего месяца
-  const totalCells = 42; // Общее количество ячеек в календаре (6 недель * 7 дней)
+  const totalCells = 42;
   const remainingCells = totalCells - firstDayOfWeek - daysInMonth;
   for (let i = 1; i <= remainingCells; i++) {
     const li = document.createElement('li');
@@ -41,15 +68,12 @@ function generateCalendar(year, month) {
   }
 }
 
-// Получаем текущую дату
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 const currentMonth = currentDate.getMonth();
 
-// Генерируем календарь для текущего месяца
 generateCalendar(currentYear, currentMonth);
 
-// Обработчики событий для изменения месяца
 const prevMonthBtn = document.querySelector('.strelaLeft');
 const nextMonthBtn = document.querySelector('.strelaRight');
 
@@ -71,4 +95,4 @@ nextMonthBtn.addEventListener('click', () => {
   generateCalendar(currentYear, currentMonth);
 });
 
-generateCalendar(2014, 4)
+generateCalendar(2013, 5)
